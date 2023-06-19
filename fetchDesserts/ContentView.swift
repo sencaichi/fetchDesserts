a24c0d542417488b4eecbcafd59e11ee79c289f2
Sen Cai <sencaichi@gmail.com>
2023-06-19 16:17:10 -0400

    deleted unused code

--- fetchDesserts/ContentView.swift
+++ fetchDesserts/ContentView.swift
@@ -6,66 +6,7 @@
 //
 
 import SwiftUI
-//
-//struct Dessert: Hashable, Codable {
-//    let strMeal: String
-//    let strMealThumb: String
-//    let idMeal: Int
-//}
-//
-//class ViewModel: ObservableObject {
-//    @Published var meals: [Dessert] = []
-//
-//    func fetch() {
-//        guard let url = URL(string: "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert") else {
-//            return
-//        }
-//
-//        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _,
-//            error in
-//            guard let data = data, error == nil else {
-//                return
-//            }
-//
-//            do {
-//                let meals = try JSONDecoder().decode([Dessert].self, from: data)
-//                DispatchQueue.main.async {
-//                    self?.meals = meals
-//                }
-//            }
-//            catch {
-//                print(error)
-//            }
-//        }
-//        task.resume()
-//    }
-//
-//}
-//
-//struct ContentView: View {
-//    @StateObject var viewModel = ViewModel()
-//
-//    var body: some View {
-//        NavigationView {
-//            List {
-//                ForEach(viewModel.meals, id: \.idMeal) { meal in
-//                    VStack {
-//                        Text("Hello")
-//                    }
-//                }
-//            }
-//            .navigationTitle("Desserts")
-//            .onAppear {viewModel.fetch()}
-//        }
-//    }
-//}
-//
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
-//
+
 struct Meal: Codable {
         let strMeal: String
         let strMealThumb: String
