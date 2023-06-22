41752f74d05606539a126853430070f4f3739f1f
Sen Cai <sencaichi@gmail.com>
2023-06-22 06:29:25 -0400

    final

---
+++ fetchDesserts/Model.swift
@@ -0,0 +1,73 @@
+//
+//  Model.swift
+//  fetchDesserts
+//
+//  Created by Sen Ayané on 6/22/23.
+//
+
+import SwiftUI
+
+// model the data to be decoded
+// nested json data addressed on line 44
+// consider using classes for objective-c interoperability
+struct Meal: Codable, Hashable {
+        let strMeal: String
+        let strMealThumb: String
+        let idMeal: String
+    }
+
+struct MealDetails: Codable, Hashable {
+    let idMeal: String
+    var strMeal: String
+    let strDrinkAlternate: String?
+    let strCategory: String?
+    let strArea: String?
+    var strInstructions: String?
+    var strMealThumb: String
+    let strTags: String?
+    let strYoutube: String?
+    var strIngredient1: String?
+    var strIngredient2: String?
+    var strIngredient3: String?
+    var strIngredient4: String?
+    var strIngredient5: String?
+    var strIngredient6: String?
+    var strIngredient7: String?
+    var strIngredient8: String?
+    var strIngredient9: String?
+    var strIngredient10: String?
+    var strIngredient11: String?
+    var strIngredient12: String?
+    var strIngredient13: String?
+    var strIngredient14: String?
+    var strIngredient15: String?
+    var strIngredient16: String?
+    var strIngredient17: String?
+    var strIngredient18: String?
+    var strIngredient19: String?
+    var strIngredient20: String?
+    let strMeasure1: String?
+    let strMeasure2: String?
+    let strMeasure3: String?
+    let strMeasure4: String?
+    let strMeasure5: String?
+    let strMeasure6: String?
+    let strMeasure7: String?
+    let strMeasure8: String?
+    let strMeasure9: String?
+    let strMeasure10: String?
+    let strMeasure11: String?
+    let strMeasure12: String?
+    let strMeasure13: String?
+    let strMeasure14: String?
+    let strMeasure15: String?
+    let strMeasure16: String?
+    let strMeasure17: String?
+    let strMeasure18: String?
+    let strMeasure19: String?
+    let strMeasure20: String?
+    let strSource: String?
+    let strImageSource: String?
+    let strCreativeCommonsConfirmed: String?
+    let dateModified: String?
+}
